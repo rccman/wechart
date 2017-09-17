492b962ca63fb5f336529e8830228da3622124c5
rencc <rcc>
2017-09-17 23:10:31 +0800

    token校验

---
+++ src/main/java/com/rencc/wechart/common/controller/CoreController.java
@@ -0,0 +1,30 @@
+package com.rencc.wechart.common.controller;
+
+import com.rencc.wechart.common.util.SignUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class CoreController {
+    //增加日志
+    private static Logger log = LoggerFactory.getLogger(CoreController.class);
+    //验证是否来自微信服务器的消息
+    @RequestMapping(value = "",method = RequestMethod.GET)
+    public String checkSignature(@RequestParam(name = "signature" ,required = false) String signature  ,
+                                 @RequestParam(name = "nonce",required = false) String  nonce ,
+                                 @RequestParam(name = "timestamp",required = false) String  timestamp ,
+                                 @RequestParam(name = "echostr",required = false) String  echostr){
+        // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
+        if (SignUtil.checkSignature(signature, timestamp, nonce)) {
+            log.info("接入成功");
+            return echostr;
+        }
+        log.error("接入失败");
+        return "";
+    }
+}
