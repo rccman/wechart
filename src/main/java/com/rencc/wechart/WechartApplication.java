f42bdfdb12ed60d17900f95929cfe5b3945589a9
任超超 <rencc@bestinvestor.com.cn>
2018-06-11 14:06:37 +0800

    dev分支

--- src/main/java/com/rencc/wechart/WechartApplication.java
+++ src/main/java/com/rencc/wechart/WechartApplication.java
@@ -2,10 +2,16 @@ package com.rencc.wechart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WechartApplication {
+public class WechartApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WechartApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(WechartApplication.class, args);
 	}
